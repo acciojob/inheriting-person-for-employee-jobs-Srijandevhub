d9bcba1227a6010bb4f84aa03fc0fc939ac42d2c
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2024-10-12 07:24:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 // complete this js code
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
-}
-Person.prototype.greet = function () {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-}
-function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
-	this.jobTitle = jobTitle;
-}
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+//Define a Person class
+// class Person(name, age){
+//     this.name = name;
+//     this.age = age;
+//   }
 
-Employee.prototype.jobGreet = function () {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 }
+
+  // greet method in Person class
+  // greet() {
+  //   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+  // }
+
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+}
+	
+
+
+
+
+// Define Employee class that extends Person
+
+//function Employee(name, age, jobTitle) {}
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    // Call the parent class constructor (Person) to initialize name and age
+    super(name, age);
+    this.jobTitle = jobTitle;
+  }
+
+  // jobGreet method in Employee class
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+  }
+	
+}
+
 
 // Creating an instance of Person
 const person = new Person("Alice", 25);
